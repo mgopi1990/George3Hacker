27d55047b2e1f41063f9eaf51079022645e38eb5
Gopinath M <31352222+mgopi1990@users.noreply.github.com>
2019-05-07 01:20:51 +0530

    is_win() check added

--- tictactoe.c
+++ tictactoe.c
@@ -2,21 +2,21 @@
 
 enum 
 {
-	TOP_LEFT	= '1',
-	TOP_MID 	= '2',
-	TOP_RIGHT	= '3',
-	MID_LEFT	= '4',
-	MID_MID		= '5',
-	MID_RIGHT	= '6',
-	BOTTOM_LEFT	= '7',
-	BOTTOM_MID	= '8',
-	BOTTOM_RIGHT = '9'
+	TOP_LEFT,    TOP_MID,    TOP_RIGHT,
+	MID_LEFT,    MID_MID,    MID_RIGHT,
+	BOTTOM_LEFT, BOTTOM_MID, BOTTOM_RIGHT
 };
 
 enum
 {
 	SUCCESS = 0, 
 	FAILURE = -1
+};
+
+enum
+{
+	TRUE = 1,
+	FALSE = 0
 };
 
 enum
@@ -28,7 +28,8 @@ enum
 enum
 {
 	XXX = 'X',
-	O00 = 'O'
+	O00 = 'O',
+	EMPTY = ' '
 };
 
 typedef struct think_
@@ -62,12 +63,11 @@ think* think_malloc(int nchild)
 
 think* construct_think_tree()
 {
-#define 
 	think *root;
 	int i = 0;
 	for (i = 0; i < TIC_TOC_SQUARES; i++)
 	{
-
+	
 	}
 }
 
@@ -121,6 +121,183 @@ think *queue_dequeue(queue *q)
 	return (tnode);		
 }
 
+int is_win(char *board, char move)
+{
+	switch(move)
+	{
+		case TOP_LEFT:
+			/*
+			 *  O|X|X
+			 *  _____
+			 *  X|X|
+			 *  _____
+			 *  X| |X
+			 */
+			if (board[TOP_LEFT] == board[TOP_MID] && 
+				board[TOP_LEFT] == board[TOP_RIGHT])
+				return TRUE;
+			else if (board[TOP_LEFT] == board[MID_LEFT] &&
+					 board[TOP_LEFT] == board[BOTTOM_LEFT])
+				return TRUE;
+			else if (board[TOP_LEFT] == board[MID_MID] &&
+					 board[TOP_LEFT] == board[BOTTOM_RIGHT])
+				return TRUE;
+			return FALSE;			
+
+		case TOP_MID:
+			/*
+			 *  X|O|X
+			 *  _____
+			 *   |X|
+			 *  _____
+			 *   |X|
+			 */
+			if (board[TOP_MID] == board[TOP_LEFT] &&
+				board[TOP_MID] == board[TOP_RIGHT])
+				return TRUE;
+			else if (board[TOP_MID] == board[MID_MID] &&
+					 board[TOP_MID] == board[BOTTOM_MID])
+				return TRUE;
+			return FALSE;
+
+		case TOP_RIGHT:
+			/*
+			 *  X|X|O
+			 *  _____
+			 *   |X|X
+			 *  _____
+			 *  X| |X
+			 *
+			 */
+			if (board[TOP_RIGHT] == board[TOP_MID] &&
+				board[TOP_RIGHT] == board[TOP_LEFT])
+				return TRUE;
+			else if (board[TOP_RIGHT] == board[MID_RIGHT] &&
+					 board[TOP_RIGHT] == board[BOTTOM_RIGHT])
+				return TRUE;
+			else if (board[TOP_RIGHT] == board[MID_MID] &&
+					 board[TOP_RIGHT] == board[BOTTOM_LEFT])
+				return TRUE;
+			return FALSE;
+			
+		case MID_LEFT:
+			/*
+			 *  X| | 
+			 *  _____
+			 *  O|X|X
+			 *  _____
+			 *  X| | 
+			 *
+			 */
+			if (board[MID_LEFT] == board[TOP_LEFT] && 
+				board[MID_LEFT] == board[BOTTOM_LEFT])
+				return TRUE;
+			else if (board[MID_LEFT] == board[MID_MID] &&
+					 board[MID_LEFT] == board[MID_RIGHT])
+				return TRUE;
+			return FALSE;			
+
+		case MID_MID:
+			/*
+			 *  X|X|X
+			 *  _____
+			 *  X|O|X
+			 *  _____
+			 *  X|X|X
+			 *
+			 */
+			if (board[MID_MID] == board[MID_LEFT] && 
+				board[MID_MID] == board[MID_RIGHT])
+				return TRUE;
+			else if (board[MID_MID] == board[TOP_MID] &&
+					 board[MID_MID] == board[BOTTOM_MID])
+				return TRUE;
+			else if (board[MID_MID] == board[TOP_LEFT] &&
+					 board[MID_MID] == board[BOTTOM_RIGHT])
+				return TRUE;
+			else if (board[MID_MID] == board[TOP_RIGHT] &&
+					 board[MID_MID] == board[BOTTOM_LEFT])
+				return TRUE;
+			return FALSE;			
+
+		case MID_RIGHT:
+			/*
+			 *   | |X
+			 *  _____
+			 *  X|X|O
+			 *  _____
+			 *   | |X
+			 *
+			 */
+			if (board[MID_RIGHT] == board[TOP_RIGHT] && 
+				board[MID_RIGHT] == board[BOTTOM_RIGHT])
+				return TRUE;
+			else if (board[MID_RIGHT] == board[MID_MID] &&
+					 board[MID_RIGHT] == board[MID_LEFT])
+				return TRUE;
+			return FALSE;			
+
+		case BOTTOM_LEFT:
+			/*
+			 *  X| |X
+			 *  _____
+			 *  X|X| 
+			 *  _____
+			 *  O|X|X
+			 *
+			 */
+			if (board[BOTTOM_LEFT] == board[MID_LEFT] && 
+				board[BOTTOM_LEFT] == board[TOP_LEFT])
+				return TRUE;
+			else if (board[BOTTOM_LEFT] == board[BOTTOM_MID] &&
+					 board[BOTTOM_LEFT] == board[BOTTOM_RIGHT])
+				return TRUE;
+			else if (board[BOTTOM_LEFT] == board[MID_MID] &&
+					 board[BOTTOM_LEFT] == board[TOP_RIGHT])
+				return TRUE;
+			return FALSE;			
+
+		case BOTTOM_MID:
+			/*
+			 *   |X| 
+			 *  _____
+			 *   |X| 
+			 *  _____
+			 *  X|O|X
+			 *
+			 */
+			if (board[BOTTOM_MID] == board[BOTTOM_LEFT] && 
+				board[BOTTOM_MID] == board[BOTTOM_RIGHT])
+				return TRUE;
+			else if (board[BOTTOM_MID] == board[MID_MID] &&
+					 board[BOTTOM_MID] == board[TOP_MID])
+				return TRUE;
+			return FALSE;			
+
+		case BOTTOM_RIGHT:
+			/*
+			 *   | |X
+			 *  _____
+			 *   | |X
+			 *  _____
+			 *  X|X|O
+			 *
+			 */
+			if (board[BOTTOM_RIGHT] == board[BOTTOM_MID] && 
+				board[BOTTOM_RIGHT] == board[BOTTOM_LEFT])
+				return TRUE;
+			else if (board[BOTTOM_RIGHT] == board[MID_RIGHT] &&
+					 board[BOTTOM_RIGHT] == board[TOP_RIGHT])
+				return TRUE;
+			else if (board[BOTTOM_RIGHT] == board[MID_MID] &&
+					 board[BOTTOM_RIGHT] == board[TOP_LEFT])
+				return TRUE;
+			return FALSE;			
+
+		default:
+			return FALSE;
+	}	
+}
 
 /* Main */
 int main()
