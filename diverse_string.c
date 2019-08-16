b8e287e4c10fe11a65e71c906b3b9f241ca4d178
Gopi <mgopi1990@gmail.com>
2019-08-16 18:25:27 +0530

    initial commit

---
+++ diverse_string.c
@@ -0,0 +1,42 @@
+/*
+ * We consider alphabet with only three letters: 'a', 'b' and 'c'. 
+ * A string is called diverse if no three consecutive letters are the same. 
+ * In other words, a diverse string may not contain any of the strings 
+ * 'aaa', 'bbb', or 'ccc'. 
+ * 
+ * Write a function,
+ *  string_diverse (int A, int B, int C);
+ * 
+ * that given three integers A, B and C, returns any longest possible diverse 
+ * string containing at most A letters 'a', at most B letters 'b' and at most 
+ * C letters 'c'. If there is no possibility of building any string, return 
+ * empty string. 
+ * 
+ * Example:
+ * 
+ * 1. Given A=6, B=1 and C=1, our function may return, 'aabaacaa'. 
+ * Note that 'aacaabaa' would also be a correct answer. 
+ * Your function may return any correct answer.
+ * 
+ * 2. Given A=1, B=3, and C=1, your function ay return 'abbcb', 'bcbab', 'bacbb' 
+ * or any serveral other strings.
+ * 
+ * 3. Given A=0, B=1 and C=8, your function should return 'ccbcc' which is the 
+ * only correct answer for this case.
+ * 
+ * Assume that,
+ *  A,B,C are integers within the range [0..100],
+ *  A+B+C > 0
+ * 
+ * In your solution focus on correctness. 
+ * The performance of your solution will not be the focus of the assessment. 
+ * 
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	
+	return 0;
+}
