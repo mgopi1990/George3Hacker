3e11614294c3c1a934b8a900e00901e004750f64
Gopi <mgopi1990@gmail.com>
2019-09-27 09:47:55 +0530

    few more approaches added

--- sort_odd_even.py
+++ sort_odd_even.py
@@ -15,27 +15,41 @@ Output: 7 5 3 1 0 2 4
 
 '''
 
+## Approach1:
+##  Just like quick sort, 
+##  move odd to one end,
+##  even to another end.
+##  now sort.
+##  This function, we tried to sort,
+##  we put the number in the correct position
+##  like in insertion sort
 def microsoft_sort(a):
 	length = len (a)
 	i = 0
 	j = length -1
 	while (i <= j):
-		print('HERE1', a, str(i), str(j))
+		#print('HERE1', a, str(i), str(j))
 		if (a[i] % 2 == 1):
 			k = i - 1
 			while (k >= 0):
-				print ('HERE3', a[k], '<', a[i])
+				#print ('HERE3', a[k], '<', a[i])
 				if (a[k] < a[i]):
-					print ('HERE2', k, i)
+					#print ('HERE2', k, i)
 					k -= 1
 					continue
+				break
 
+			if (k < 0):
+				k = 0
+			else:
+				k += 1
+
+			if (k != i):
 				temp = a[i]
 				del(a[i])
-				print ('DEL', i, temp)
+				#print ('DEL', i, temp)
 				a.insert(k, temp)
-				print ('INSERT', a, k, temp)
-				break
+				#print ('INSERT', a, k, temp)
 		else:
 			k = length -1
 			while (k > j):
@@ -50,8 +64,48 @@ def microsoft_sort(a):
 		i += 1
 	return (a)
 
+## Since its told that all numbers are positive.
+## Lets just multiply -1 with odd numbers.
+## Naturally biggest odd number will become
+## smallest negative number
+def microsoft_sort2(a):
+	## Oneliner :-P  But worst. Initializes two array
+	return [-x if (x&0x01) else x for x in sorted([-x if (x&0x01) else x for x in a ])]
+
+## Better version of sort2
+## should use 2's complement ?? for -x ?
+## just one list with few extra lines
+def microsoft_sort3(a):
+	i = 0
+	while (i < len(a)):
+		## odd number
+		if (a[i] & 0x01):
+			temp = -a[i]
+		else:
+			temp = a[i]
+
+		## perform insertion sort
+		j = 0
+		while (j < i):
+			if (a[j] > temp):
+				del(a[i])
+				a.insert(j, temp)
+				break
+			j += 1
+		i += 1
+
+	## now remove - for odd numbers
+	j = 0
+	while (j < len(a)):
+		if (a[j] & 0x01 == 0):
+			break;
+		a[j] = -a[j]
+		j += 1
+
+	return a
+
 if __name__ == "__main__":
 	inp = input('Input: ');
-	output = microsoft_sort ([int(x) for x in inp.split(' ')])
+	output = microsoft_sort3 ([int(x) for x in inp.split(' ')])
 	print ('Output: ' + ' '.join([str(x) for x in output]))
 
