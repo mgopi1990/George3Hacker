03a35743e67bb40d7058f369e1e1506f16ac5013
Gopinath M <mgopi1990@gmail.com>
2021-02-07 01:13:56 +0530

    merge_dragon first commit

---
+++ merge_dragon.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+## Lets check how much merge is needed for different levels
+## in dragon merge.
+## lets check 3 merge line and 5 merge line,
+## and see like, how long it takes to reach greater levels
+
+MAX_LEVEL = 25 
+
+if __name__ == "__main__":
+    
+    for level in range(1, 25+1):
+
